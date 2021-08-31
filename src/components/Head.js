7d921fc5d7b6e0d5227d7d515bf01dbe7efa0e68
Sid <sidchopp@gmail.com>
2021-08-31 13:25:28 -0600

    head included

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Header, Image, Icon, Divider, Segment } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
-function Head() {
-  return (
-    <div>
-      I am header
-    </div>
-  )
-}
+const Head = () => (
+  <div >
+    <Segment style={{ backgroundColor: '#c59d5f' }} clearing>
+      <Header style={{ color: 'black' }} floated='left' as='h1' icon='utensils' content=' La cuisine de Purva' />
+      <Header as='h2' floated='right'>
+        <a href="https://github.com/sidchopp/menu" target="_blank"> <Icon color='black' name=' github square' size='large' /></a>
+        <a href="https://www.linkedin.com/in/sidchopp/" target="_blank"> <Icon color='black' name='linkedin' size='large' /></a>
+      </Header>
+
+    </Segment>
+
+  </div>
+)
 
 export default Head
